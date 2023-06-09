4200c7903e8f6e516b12a02d5dc665083d4a7fc6
Boguslaw Drozd <b.drozd07@gmail.com>
2023-06-09 22:26:45 +0200

    InputFormValidate

--- js/index.js
+++ js/index.js
@@ -30,16 +30,20 @@ inputIncomeButton.addEventListener("click", incomeButtonHandleClick);
 
 function incomeButtonHandleClick(event) {
   event.preventDefault();
+  if (inputIncomeAmount.value < 0) {
+    clearIncomeForm();
+    return;
+  }
   const incomeName = inputIncomeName.value;
   const incomeAmount = inputIncomeAmount.value;
-  const incomeID = `item-${incomesArray.length}`;
+  const incomeID = `incomeItem-${incomesArray.length}`;
   incomesArray.push({
     incomeName: incomeName,
     incomeAmount: incomeAmount,
     incomeID: incomeID,
   });
   updateListOfIncomes();
-  const sumOfIncomes = updateSumOfIncomes();
+  const sumOfIncomes = updateSumOfIncomes().toFixed(2);
   balanceTableIncomesSummary.innerHTML = "";
   const sumOfIncomesH1 = document.createElement("h1");
   sumOfIncomesH1.innerText = `SUMA PRZYCHODÓW: ${sumOfIncomes} ${currency}`;
@@ -49,12 +53,12 @@ function incomeButtonHandleClick(event) {
   updateBalanceSheetResult();
   clearIncomeForm();
 }
-function updateSumOfIncomes(){
+function updateSumOfIncomes() {
   const incomesArrayValues = updateIncomesArrayValue();
   const sumOfIncomes = incomesArrayValues.reduce((acc, number) => {
     return acc + number;
   }, 0);
-  
+
   return sumOfIncomes;
 }
 
@@ -77,12 +81,16 @@ function updateListOfIncomes() {
       index
     ].textContent = `${income.incomeName} - ${income.incomeAmount} ${currency}`;
     incomeItem[index].id = `${income.incomeID}`;
+    const incomeButtons = listOfIncomes.querySelectorAll("button");
+    incomeButtons[0].id = `incomeEditButton-${income.incomeID}`;
+    incomeButtons[1].id = `incomeDeleteButton-${income.incomeID}`;
   });
 }
 function updateIncomesArrayValue() {
   const incomesArrayValues = [];
   incomesArray.forEach((income, index) => {
     const incomeAsNumber = parseFloat(income.incomeAmount);
+
     incomesArrayValues.push(incomeAsNumber);
   });
   return incomesArrayValues;
@@ -103,17 +111,20 @@ inputExpenseButton.addEventListener("click", expenseButtonHandleClick);
 
 function expenseButtonHandleClick(event) {
   event.preventDefault();
-
+  if (inputExpenseAmount.value < 0) {
+    clearExpenseForm();
+    return;
+  }
   const expenseName = inputExpenseName.value;
   const expenseAmount = inputExpenseAmount.value;
-  const expenseID = `item-${expensesArray.length}`;
+  const expenseID = `expenseItem-${expensesArray.length}`;
   expensesArray.push({
     expenseName: expenseName,
     expenseAmount: expenseAmount,
     expenseID: expenseID,
   });
   updateListOfExpenses();
-  const sumOfExpenses = updateSumOfExpenses();
+  const sumOfExpenses = updateSumOfExpenses().toFixed(2);
   balanceTableExpensesSummary.innerHTML = "";
   const sumOfExpensesH1 = document.createElement("h1");
   sumOfExpensesH1.innerText = `SUMA WYDATKÓW: ${sumOfExpenses} ${currency}`;
@@ -143,6 +154,9 @@ function updateListOfExpenses() {
       index
     ].textContent = `${expense.expenseName} - ${expense.expenseAmount} ${currency}`;
     expenseItem[index].id = `${expense.expenseID}`;
+    const expenseButtons = listOfExpenses.querySelectorAll("button");
+    expenseButtons[0].id = `expenseEditButton-${expense.expenseID}`;
+    expenseButtons[1].id = `expenseDeleteButton-${expense.expenseID}`;
   });
 }
 function updateExpensesArrayValue() {
@@ -153,12 +167,12 @@ function updateExpensesArrayValue() {
   });
   return expensesArrayValues;
 }
-function updateSumOfExpenses(){
+function updateSumOfExpenses() {
   const expensesArrayValues = updateExpensesArrayValue();
   const sumOfExpenses = expensesArrayValues.reduce((acc, number) => {
     return acc + number;
   }, 0);
-  
+
   return sumOfExpenses;
 }
 //end of Expenses
@@ -179,42 +193,38 @@ const positiveBalanceText = "BILANS DODATNI MOŻESZ JESZCZE WYDAĆ:";
 const negativeBalanceText = "BILANS UJEMNY JESTEŚ NA MINUSIE:";
 const currency = "zł";
 
-function updateBalanceSheetResult(){
-
+function updateBalanceSheetResult() {
   const sumOfIncomes = updateSumOfIncomes();
   const sumOfExpenses = updateSumOfExpenses();
-  const balanceSheetResult = sumOfIncomes - sumOfExpenses;
-  balanceSheetResultField.innerHTML ="";
-let balanceIndex = "";
-if (balanceSheetResult == 0) {
-  balanceIndex = "balance";
-} else if (balanceSheetResult > 0) {
-  balanceIndex = "positive";
-} else {
-  balanceIndex = "negative";
-}
+  const balanceSheetResult = (sumOfIncomes - sumOfExpenses).toFixed(2);
+  balanceSheetResultField.innerHTML = "";
+  let balanceIndex = "";
+  if (balanceSheetResult == 0) {
+    balanceIndex = "balance";
+  } else if (balanceSheetResult > 0) {
+    balanceIndex = "positive";
+  } else {
+    balanceIndex = "negative";
+  }
 
-let summaryText = " ";
-switch (balanceIndex) {
-  case "balance":
-    summaryText = `${neutralBalanceText}`;
-    break;
-  case "positive":
-    summaryText = `${positiveBalanceText} ${balanceSheetResult} ${currency}`;
-    break;
-  case "negative":
-    summaryText = `${negativeBalanceText} ${balanceSheetResult} ${currency}`;
-    break;
-  default:
-    summaryText = "WPROWADŹ POPRAWNE DANE";
-}
+  let summaryText = " ";
+  switch (balanceIndex) {
+    case "balance":
+      summaryText = `${neutralBalanceText}`;
+      break;
+    case "positive":
+      summaryText = `${positiveBalanceText} ${balanceSheetResult} ${currency}`;
+      break;
+    case "negative":
+      summaryText = `${negativeBalanceText} ${balanceSheetResult} ${currency}`;
+      break;
+    default:
+      summaryText = "WPROWADŹ POPRAWNE DANE";
+  }
 
-
-
-
-const balanceSheetResultH1 = document.createElement("h1");
-balanceSheetResultH1.innerText = summaryText;
-balanceSheetResultH1.classList.add("balanceSheetResult");
-balanceSheetResultField.appendChild(balanceSheetResultH1);
+  const balanceSheetResultH1 = document.createElement("h1");
+  balanceSheetResultH1.innerText = summaryText;
+  balanceSheetResultH1.classList.add("balanceSheetResult");
+  balanceSheetResultField.appendChild(balanceSheetResultH1);
 }
 //End of creation of the balanceSheetResult element.
